be4311731b752adfc94d9953adf17c1987bac643
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2022-04-13 21:31:33 -0400

    redlines list file

---
+++ src/Redlines.h
@@ -0,0 +1,16 @@
+//
+// Created by kevin on 4/13/2022.
+//
+
+#ifndef BETTER_ENGINE_CONTROL_SOFTWARE_REDLINES_H
+#define BETTER_ENGINE_CONTROL_SOFTWARE_REDLINES_H
+
+#include "Redline.h"
+#include "ECSUtils.h"
+#include <vector>
+
+std::vector<Redline*> LOX_FILL_REDLINES;
+
+
+
+#endif //BETTER_ENGINE_CONTROL_SOFTWARE_REDLINES_H
