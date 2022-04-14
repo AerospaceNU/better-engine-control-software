a9906e98f904de988dd6c628ae9363a1e2964ae9
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2022-04-13 21:58:25 -0400

    merged online safe redlines

--- src/Redlines.h
+++ src/Redlines.h
@@ -14,6 +14,7 @@
 
 
 std::vector<Redline *> ONLINE_SAFE_D = {
+        //REMOTELY CONTROLLED VALVES
         new ValveRedline("loxVent", [](SensorData *data) { return data->loxVent; }, OPEN),
         new ValveRedline("kerVent", [](SensorData *data) { return data->kerVent; }, OPEN),
         new ValveRedline("loxDrip", [](SensorData *data) { return data->loxDrip; }, OPEN),
@@ -24,69 +25,6 @@ std::vector<Redline *> ONLINE_SAFE_D = {
         new ValveRedline("kerFlow", [](SensorData *data) { return data->kerFlow; }, CLOSED),
         new ValveRedline("loxPurge", [](SensorData *data) { return data->loxPurge; }, CLOSED),
         new ValveRedline("kerPurge", [](SensorData *data) { return data->kerPurge; }, CLOSED),
-}
-
-new IntWithinRedline("loxTankDucer",
-                     [](SensorData* data){return data->loxTankDucer;},
-                     0, 15);
-new IntWithinRedline("kerTankDucer",
-                    [](SensorData* data){return data->kerTankDucer;},
-                    0, 15);
-
-new IntWithinRedline("purgeDucer",
-                    [](SensorData* data){return data->purgeDucer;},
-                    0, 500);
-new IntWithinRedline("loxInletDucer",
-[](SensorData* data){return data->loxInletDucer;},
-0, 15);
-new IntWithinRedline("manifoldInletDucer",
-[](SensorData* data){return data->manifoldInletDucer;},
-0, 15);
-new IntWithinRedline("kerInletDucer",
-[](SensorData* data){return data->kerInletDucer;},
-0, 15);
-new IntWithinRedline("loxVenturi",
-[](SensorData* data){return data->loxVenturi;},
-0, 15);
-new IntWithinRedline("kerVenturi",
-[](SensorData* data){return data->kerVenturi;},
-0, 15);
-
-new IntWithinRedline("loadcell",
-[](SensorData* data){return data->loadcell;},
-0, 15);
-
-new IntWithinRedline("loxTank1",
-[](SensorData* data){return data->loxTank1;},
-20, 30);
-new IntWithinRedline("loxTank2",
-[](SensorData* data){return data->loxTank2;},
-20, 30);
-new IntWithinRedline("loxTank3",
-[](SensorData* data){return data->loxTank3;},
-20, 30);
-new IntWithinRedline("loxDripLine",
-[](SensorData* data){return data->loxDripLine;},
-20, 30);
-new IntWithinRedline("outsideThroat",
-[](SensorData* data){return data->outsideThroat;},
-20, 30);
-new IntWithinRedline("nozzle",
-[](SensorData* data){return data->nozzle;},
-20, 30);
-
-
-
-        new ValveRedline("loxVent", [](SensorData *data) { return data->sensor1Data; }, OPEN),
-        new ValveRedline("kerVent", [](SensorData *data) { return data->sensor2Data; }, OPEN),
-        new ValveRedline("loxDrip", [](SensorData *data) { return data->sensor3Data; }, OPEN),
-        new ValveRedline("kerDrip", [](SensorData *data) { return data->sensor3Data; }, OPEN),
-        new ValveRedline("loxPressurant", [](SensorData *data) { return data->sensor4Data; }, CLOSED),
-        new ValveRedline("kerPressurant", [](SensorData *data) { return data->sensor5Data; }, CLOSED),
-        new ValveRedline("loxFlow", [](SensorData *data) { return data->sensor6Data; }, CLOSED),
-        new ValveRedline("kerFlow", [](SensorData *data) { return data->sensor7Data; }, CLOSED),
-        new ValveRedline("loxPurge", [](SensorData *data) { return data->sensor8Data; }, CLOSED),
-        new ValveRedline("kerPurge", [](SensorData *data) { return data->sensor9Data; }, CLOSED),
         //PRESSURE SENSORS (psi)
         new IntWithinRedline("loxTankDucer",
                              [](SensorData *data) { return data->loxTankDucer; },
@@ -135,8 +73,7 @@ new IntWithinRedline("nozzle",
         new IntWithinRedline("nozzle",
                              [](SensorData *data) { return data->nozzle; },
                              20, 30)
-}
-
+};
 
 
 #endif //BETTER_ENGINE_CONTROL_SOFTWARE_REDLINES_H
