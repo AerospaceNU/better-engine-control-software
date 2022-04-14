960ee1ada87b29b41767476f509d0b8c51670bd4
alllison cross <67401718+alllisonc@users.noreply.github.com>
2022-04-13 21:45:55 -0400

    Update Redlines.h
    online safe valves

--- src/Redlines.h
+++ src/Redlines.h
@@ -9,8 +9,21 @@
 #include "ECSUtils.h"
 #include <vector>
 
-std::vector<Redline*> LOX_FILL_REDLINES;
 
+std::vector<Redline *> LOX_FILL_REDLINES;
+std::vector<Redline *> ONLINE_SAFE_D;
+ONLINE_SAFE_D = {
+        new ValveRedline("loxVent", [](SensorData *data) { return data->sensor1Data; }, OPEN),
+        new ValveRedline("kerVent", [](SensorData *data) { return data->sensor2Data; }, OPEN),
+        new ValveRedline("loxDrip", [](SensorData *data) { return data->sensor3Data; }, OPEN),
+        new ValveRedline("kerDrip", [](SensorData *data) { return data->sensor3Data; }, OPEN),
+        new ValveRedline("loxPressurant", [](SensorData *data) { return data->sensor4Data; }, CLOSED),
+        new ValveRedline("kerPressurant", [](SensorData *data) { return data->sensor5Data; }, CLOSED),
+        new ValveRedline("loxFlow", [](SensorData *data) { return data->sensor6Data; }, CLOSED),
+        new ValveRedline("kerFlow", [](SensorData *data) { return data->sensor7Data; }, CLOSED),
+        new ValveRedline("loxPurge", [](SensorData *data) { return data->sensor8Data; }, CLOSED),
+        new ValveRedline("kerPurge", [](SensorData *data) { return data->sensor9Data; }, CLOSED),
+}
 
 
 #endif //BETTER_ENGINE_CONTROL_SOFTWARE_REDLINES_H
