5cf7633cb33588e62ea8af7d9bfe647a474c4ca7
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2022-04-13 21:00:30 -0400

    sensordata field placeholders

--- src/SensorData.h
+++ src/SensorData.h
@@ -4,6 +4,7 @@
 #ifndef BETTER_ENGINE_CONTROL_SOFTWARE_SENSORDATA_H
 #define BETTER_ENGINE_CONTROL_SOFTWARE_SENSORDATA_H
 
+#include "ECSUtils.h"
 
 class SensorData{
 public:
@@ -13,9 +14,37 @@ public:
     int sensor3Data;
     int sensor4Data;
 
+    //REMOTELY CONTROLLED VALVES
+    ECSValveState loxVent = OPEN;
+    ECSValveState kerVent = CLOSED;
+    ECSValveState loxDrip = INVALID;
+    ECSValveState kerDrip = OPEN;
+    ECSValveState loxPressurant = CLOSED;
+    ECSValveState kerPressurant = INVALID;
+    ECSValveState loxFlow = OPEN;
+    ECSValveState kerFlow = CLOSED;
+    ECSValveState loxPurge = INVALID;
+    ECSValveState kerPurge = OPEN;
 
-protected:
+    //PRESSURE SENSORS (psi)
+    int loxTankDucer = 15;
+    int kerTankDucer = 15;
+    int purgeDucer = 15;
+    int loxInletDucer = 15;
+    int manifoldInletDucer = 15;
+    int kerInletDucer = 15;
+    int loxVenturi = 15;
+    int kerVenturi = 15;
 
+    //LOADCELL (lbf)
+    int loadCell = 15;
 
+    //TEMP SENSORS (C)
+    int loxTank1 = 25;
+    int loxTank2 = 25;
+    int loxTank3 = 25;
+    int loxDripLine = 25;
+    int outsideThroat = 25;
+    int nozzle = 25;
 };
 #endif //BETTER_ENGINE_CONTROL_SOFTWARE_SENSORDATA_H
