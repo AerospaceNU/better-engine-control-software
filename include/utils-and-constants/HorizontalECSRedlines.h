d9f463eb151a5401497920dfc1bc8d0b2afc79c3
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2022-05-27 20:52:58 -0400

    refactored redline vectors to be extern vars

---
+++ include/utils-and-constants/HorizontalECSRedlines.h
@@ -0,0 +1,12 @@
+#ifndef BETTER_ENGINE_CONTROL_SOFTWARE_REDLINES_H
+#define BETTER_ENGINE_CONTROL_SOFTWARE_REDLINES_H
+
+#include "ValveRedline.h"
+#include "IntWithinRedline.h"
+#include <vector>
+
+//extern const std::vector<IRedline *> ONLINE_SAFE_D;
+
+extern const std::vector<IRedline *> ONLINE_SAFE_D;
+
+#endif //BETTER_ENGINE_CONTROL_SOFTWARE_REDLINES_H
