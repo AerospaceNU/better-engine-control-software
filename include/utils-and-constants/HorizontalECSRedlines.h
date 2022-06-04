c65a72d31a9bdb9e6d486db59631803b0d3cd97f
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2022-06-04 18:35:10 -0400

    list of redlines

--- include/utils-and-constants/HorizontalECSRedlines.h
+++ include/utils-and-constants/HorizontalECSRedlines.h
@@ -5,7 +5,19 @@
 #include "IntWithinRedline.h"
 #include <vector>
 
+//extern const std::vector<const IRedline*> ONLINE_SAFE_D;
+
 extern const std::vector<const IRedline*> ONLINE_SAFE_D;
+extern const std::vector<const IRedline*> KERO_FILL;
+extern const std::vector<const IRedline*> KERO_FILLED;
+extern const std::vector<const IRedline*> LOX_FILL;
+extern const std::vector<const IRedline*> LOX_FILLED;
+extern const std::vector<const IRedline*> ONLINE_SAFE;
+extern const std::vector<const IRedline*> READY_TO_PRESSURIZE;
+extern const std::vector<const IRedline*> PRESSURIZING;
+extern const std::vector<const IRedline*> FIRE;
+extern const std::vector<const IRedline*> ENGINE_PURGE;
+extern const std::vector<const IRedline*> TANK_PURGE;
 
 //extern const std::vector<const std::reference_wrapper<IRedline>> ONLINE_SAFE_D;
 
