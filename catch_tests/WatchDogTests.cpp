5e381a7ccf2249651c3cc4808d98facb484c0338
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2022-06-16 11:57:35 -0400

    catch2 setup

--- catch_tests/WatchDogTests.cpp
+++ catch_tests/WatchDogTests.cpp
@@ -2,11 +2,11 @@
 // Created by kevin on 6/8/2022.
 //
 
-#define CATCH_CONFIG_MAIN
+
 #include "catch.hpp"
 #include "WatchDog.h"
 
-TEST_CASE("ExampleDate", "[AbsoluteDateTests]"){  // 12/2/2020 -> 737761
+TEST_CASE("ExampleDate", "[WatchDog]"){  // 12/2/2020 -> 737761
 
     CHECK(0 == 0);
 }
