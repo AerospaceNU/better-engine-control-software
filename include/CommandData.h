1295210970102467238b7ca07aa4fb10216cc92d
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2022-06-16 11:57:56 -0400

    commanddata struct thing

--- include/CommandData.h
+++ include/CommandData.h
@@ -4,7 +4,21 @@
 
 #ifndef BETTER_ENGINE_CONTROL_SOFTWARE_COMMANDDATA_H
 #define BETTER_ENGINE_CONTROL_SOFTWARE_COMMANDDATA_H
-class CommandData {
 
+#include "utils-and-constants/ECSUtils.h"
+
+class CommandData {
+public:
+    //REMOTELY CONTROLLED VALVES
+    ECSValveState loxVent = ECSValveState::OPEN;
+    ECSValveState kerVent = ECSValveState::CLOSED;
+    ECSValveState loxDrip = ECSValveState::INVALID;
+    ECSValveState kerDrip = ECSValveState::OPEN;
+    ECSValveState loxPressurant = ECSValveState::CLOSED;
+    ECSValveState kerPressurant = ECSValveState::INVALID;
+    ECSValveState loxFlow = ECSValveState::OPEN;
+    ECSValveState kerFlow = ECSValveState::CLOSED;
+    ECSValveState loxPurge = ECSValveState::INVALID;
+    ECSValveState kerPurge = ECSValveState::OPEN;
 };
 #endif //BETTER_ENGINE_CONTROL_SOFTWARE_COMMANDDATA_H
