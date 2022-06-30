37a8953986c8f9688b049a3f817d57b9afb40627
Kevin Huang <41244296+kh9sd@users.noreply.github.com>
2022-06-30 17:41:29 -0400

    tweaked sensordata fields, wrong sensor name

--- include/SensorData.h
+++ include/SensorData.h
@@ -9,36 +9,40 @@
 class SensorData{
 public:
     //REMOTELY CONTROLLED VALVES
-    ECSValveState loxVent = ECSValveState::OPEN;
-    ECSValveState kerVent = ECSValveState::CLOSED;
+    ECSValveState loxVent = ECSValveState::INVALID;
+    ECSValveState kerVent = ECSValveState::INVALID;
     ECSValveState loxDrip = ECSValveState::INVALID;
-    ECSValveState kerDrip = ECSValveState::OPEN;
-    ECSValveState loxPressurant = ECSValveState::CLOSED;
+    ECSValveState kerDrip = ECSValveState::INVALID;
+    ECSValveState loxPressurant = ECSValveState::INVALID;
     ECSValveState kerPressurant = ECSValveState::INVALID;
-    ECSValveState loxFlow = ECSValveState::OPEN;
-    ECSValveState kerFlow = ECSValveState::CLOSED;
+    ECSValveState loxFlow = ECSValveState::INVALID;
+    ECSValveState kerFlow = ECSValveState::INVALID;
     ECSValveState loxPurge = ECSValveState::INVALID;
-    ECSValveState kerPurge = ECSValveState::OPEN;
+    ECSValveState kerPurge = ECSValveState::INVALID;
 
     //PRESSURE SENSORS (psi)
-    int loxTankDucer = 15;
-    int kerTankDucer = 15;
-    int purgeDucer = 15;
-    int loxInletDucer = 15;
-    int manifoldInletDucer = 15;
-    int kerInletDucer = 15;
-    int loxVenturi = 15;
-    int kerVenturi = 15;
+    int loxTankDucer = 0; // ADC 11
+    int kerTankDucer = 0; //ADC 8
+    int purgeDucer = 0; // ADC 6
+    int loxInletDucer = 0; // ADC 4
+    int kerInletDucer = 0; // ADC 5
+    int kerPintleDucer = 0; // ADC 7
+    int loxVenturi = 0; // ADC 2
+    int kerVenturi = 0; // ADC 3
+
+    int pnematicsDucer = 0; // ADC 10
+    // unused adcs: lox reg (0), kero reg (1), do not use (9)
 
     //LOADCELL (lbf)
-    int loadCell = 15;
+    int loadCell = 0; // direct field on packet
 
     //TEMP SENSORS (C)
-    int loxTank1 = 25;
-    int loxTank2 = 25;
-    int loxTank3 = 25;
-    int loxDripLine = 25;
-    int outsideThroat = 25;
-    int nozzle = 25;
+    // not currently being measuring properly, ignore
+    int loxTank1 = 0;
+    int loxTank2 = 0;
+    int loxTank3 = 0;
+    int loxDripLine = 0;
+    int outsideThroat = 0;
+    int nozzle = 0;
 };
 #endif //BETTER_ENGINE_CONTROL_SOFTWARE_SENSORDATA_H
