bf2a26e2897a198ee38ef0a251c928a8bc522014
Kevin Huang <kevin9sd@gmail.com>
2022-08-12 21:53:23 -0400

    catch runner documentation

--- catch_tests/catch_runner.cpp
+++ catch_tests/catch_runner.cpp
@@ -1,10 +1,10 @@
-//
-// Created by kevin on 6/16/2022.
-//
-
+/**
+ * This file basically defines a main method for the Catch2 tests
+ *
+ * To use it, add to it a executable and link it to a lib of tests
+ *
+ * Do NOT do "#define CATCH_CONFIG_MAIN" in other file, this will
+ * cause horrible linker errors
+ */
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-
-//TEST_CASE("bruh", "[bro, wtf]"){
-//    REQUIRE(false);
-//}
