c38d6e829cba8b1d9be0d51b47b43de5531365d3
Kevin Huang <kevin9sd@gmail.com>
2022-09-19 21:25:41 -0400

    comm-boundary unit tests

---
+++ catch_tests/comm-boundary/ParsingHelperTests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by kevin on 9/19/2022.
+//
+
+#include "../catch.hpp"
+#include "comm-boundary/ParsingHelpers.h"
+
+TEST_CASE("valveStateToString", "[unit]"){
+    REQUIRE(0 == 0);
+}
