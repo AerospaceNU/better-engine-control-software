07c149088f703c129b295dce1ea2b221df6993e9
Kevin Huang <kevin9sd@gmail.com>
2023-09-15 20:38:00 +0000

    Document failed RTTI removal attempt
    Found a way to remove RTTI usage on websockets, but LibSerial unconditionally uses it
    
    * remove an unnecessary include dependency
    
    * doc
    
    * recomment out rrti flags, note libserial is the hold up point
    
    * turn on no rtti flags
    
    * define macros to stop typeid in asio

--- src/comm-boundary/SocketLogger.h
+++ src/comm-boundary/SocketLogger.h
@@ -1,16 +1,25 @@
 #ifndef ENGINECONTROLSYSTEM_NETWORKER_H
 #define ENGINECONTROLSYSTEM_NETWORKER_H
 
-#include "ecs/IECS.h"
 #include "ICommBoundary.h"
 
 #include <set>
 #include <json.hpp>
+
+// flags to switch off use of RTTI (typeid in this case) for whatever asio lib websocketpp is using
+#define BOOST_ASIO_NO_TYPEID
+#define ASIO_NO_TYPEID
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
+#undef ASIO_NO_TYPEID
+#undef BOOST_ASIO_NO_TYPEID
+
 #include "utils/ThreadQueue.h"
 
 #include "logger/Logger.h"
+
+// forward declaration lmao
+class IECS;
 
 using json = nlohmann::json;
 
