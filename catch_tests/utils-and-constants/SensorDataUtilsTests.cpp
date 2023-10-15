a54ce14d8658e8b311dc04a09e2d003ec4606d73
Kevin Huang <kevin9sd@gmail.com>
2023-10-15 18:01:07 -0400

    refactor test structure a bit

--- catch_tests/utils-and-constants/SensorDataUtilsTests.cpp
+++ catch_tests/utils-and-constants/SensorDataUtilsTests.cpp
@@ -12,8 +12,10 @@ TEST_CASE("SensorData median tests", "[unit]"){
     static_assert(SensorData::majorVersion == 4,
                   "Test not updated from SensorData change, please update this function and the static_assert");
     SECTION("Multiple SensorDatas"){
-        SensorData data1;
+        std::vector<SensorData> input;
+
         {
+            SensorData data1;
             data1.loxVent = ECSValveState::OPEN;
             data1.kerVent = ECSValveState::OPEN;
             data1.loxDrip = ECSValveState::OPEN;
@@ -49,10 +51,12 @@ TEST_CASE("SensorData median tests", "[unit]"){
             data1.kerOutletTC_Fault = 1;
             data1.loxTankTC_Fault = 1;
             data1.miscTC_Fault = 1;
+
+            input.emplace_back(data1);
         }
 
-        SensorData data2;
         {
+            SensorData data2;
             data2.loxVent = ECSValveState::CLOSED;
             data2.kerVent = ECSValveState::CLOSED;
             data2.loxDrip = ECSValveState::CLOSED;
@@ -88,10 +92,12 @@ TEST_CASE("SensorData median tests", "[unit]"){
             data2.kerOutletTC_Fault = 1;
             data2.loxTankTC_Fault = 1;
             data2.miscTC_Fault = 1;
+
+            input.emplace_back(data2);
         }
 
-        SensorData data3;
         {
+            SensorData data3;
             data3.loxVent = ECSValveState::CLOSED;
             data3.kerVent = ECSValveState::CLOSED;
             data3.loxDrip = ECSValveState::CLOSED;
@@ -127,49 +133,48 @@ TEST_CASE("SensorData median tests", "[unit]"){
             data3.kerOutletTC_Fault = 0;
             data3.loxTankTC_Fault = 0;
             data3.miscTC_Fault = 0;
+
+            input.emplace_back(data3);
         }
 
-        SECTION("Median SensorData test"){
-            std::vector<SensorData> input = {data1, data2, data3};
+        SensorData result = medianData(input);
 
-            SensorData result = medianData(input);
+        REQUIRE(result.loxVent == ECSValveState::CLOSED);
+        REQUIRE(result.kerVent == ECSValveState::CLOSED);
+        REQUIRE(result.loxDrip == ECSValveState::CLOSED);
+        REQUIRE(result.kerDrip == ECSValveState::CLOSED);
+        REQUIRE(result.loxPressurant == ECSValveState::CLOSED);
+        REQUIRE(result.kerPressurant == ECSValveState::CLOSED);
+        REQUIRE(result.loxFlow == ECSValveState::CLOSED);
+        REQUIRE(result.kerFlow == ECSValveState::CLOSED);
+        REQUIRE(result.loxPurge == ECSValveState::CLOSED);
+        REQUIRE(result.kerPurge == ECSValveState::CLOSED);
+        REQUIRE(result.kerOrifice == ECSValveState::CLOSED);
 
-            REQUIRE(result.loxVent == ECSValveState::CLOSED);
-            REQUIRE(result.kerVent == ECSValveState::CLOSED);
-            REQUIRE(result.loxDrip == ECSValveState::CLOSED);
-            REQUIRE(result.kerDrip == ECSValveState::CLOSED);
-            REQUIRE(result.loxPressurant == ECSValveState::CLOSED);
-            REQUIRE(result.kerPressurant == ECSValveState::CLOSED);
-            REQUIRE(result.loxFlow == ECSValveState::CLOSED);
-            REQUIRE(result.kerFlow == ECSValveState::CLOSED);
-            REQUIRE(result.loxPurge == ECSValveState::CLOSED);
-            REQUIRE(result.kerPurge == ECSValveState::CLOSED);
-            REQUIRE(result.kerOrifice == ECSValveState::CLOSED);
+        REQUIRE(result.loxTankDucer == 7);
+        REQUIRE(result.kerTankDucer == 7);
+        REQUIRE(result.purgeDucer == 7);
+        REQUIRE(result.loxInletDucer == 7);
+        REQUIRE(result.kerInletDucer == 7);
+        REQUIRE(result.kerPintleDucer == 7);
+        REQUIRE(result.loxVenturi == 7);
+        REQUIRE(result.kerVenturi == 7);
+        REQUIRE(result.pneumaticDucer == 7);
+        REQUIRE(result.loadCell == 7);
+        REQUIRE(result.loxRegDucer == 7);
+        REQUIRE(result.kerRegDucer == 7);
+        REQUIRE(result.n2pressDucer == 7);
+        REQUIRE(result.loxTankTC == 7);
+        REQUIRE(result.kerInletTC == 7);
+        REQUIRE(result.kerOutletTC == 7);
+        REQUIRE(result.miscTC == 7);
+        REQUIRE(result.boardTemp == 7);
 
-            REQUIRE(result.loxTankDucer == 7);
-            REQUIRE(result.kerTankDucer == 7);
-            REQUIRE(result.purgeDucer == 7);
-            REQUIRE(result.loxInletDucer == 7);
-            REQUIRE(result.kerInletDucer == 7);
-            REQUIRE(result.kerPintleDucer == 7);
-            REQUIRE(result.loxVenturi == 7);
-            REQUIRE(result.kerVenturi == 7);
-            REQUIRE(result.pneumaticDucer == 7);
-            REQUIRE(result.loadCell == 7);
-            REQUIRE(result.loxRegDucer == 7);
-            REQUIRE(result.kerRegDucer == 7);
-            REQUIRE(result.n2pressDucer == 7);
-            REQUIRE(result.loxTankTC == 7);
-            REQUIRE(result.kerInletTC == 7);
-            REQUIRE(result.kerOutletTC == 7);
-            REQUIRE(result.miscTC == 7);
-            REQUIRE(result.boardTemp == 7);
+        REQUIRE(result.kerInletTC_Fault == 0);
+        REQUIRE(result.kerOutletTC_Fault == 0);
+        REQUIRE(result.loxTankTC_Fault == 0);
+        REQUIRE(result.miscTC_Fault == 0);
 
-            REQUIRE(result.kerInletTC_Fault == 0);
-            REQUIRE(result.kerOutletTC_Fault == 0);
-            REQUIRE(result.loxTankTC_Fault == 0);
-            REQUIRE(result.miscTC_Fault == 0);
-        }
     }
 
     SECTION("Empty SensorDatas"){
@@ -225,81 +230,87 @@ TEST_CASE("SensorData average tests", "[unit]"){\
 
     SECTION("Multiple SensorDatas")
     {
-        SensorData data1;
-        data1.loxVent = ECSValveState::OPEN;
-        data1.kerVent = ECSValveState::OPEN;
-        data1.loxDrip = ECSValveState::OPEN;
-        data1.kerDrip = ECSValveState::OPEN;
-        data1.loxPressurant = ECSValveState::OPEN;
-        data1.kerPressurant = ECSValveState::OPEN;
-        data1.loxFlow = ECSValveState::OPEN;
-        data1.kerFlow  = ECSValveState::OPEN;
-        data1.loxPurge = ECSValveState::OPEN;
-        data1.kerPurge = ECSValveState::OPEN;
-        data1.kerOrifice = ECSValveState::OPEN;
+        std::vector<SensorData> input;
 
-        data1.loxTankDucer = 5;
-        data1.kerTankDucer = 5;
-        data1.purgeDucer = 5;
-        data1.loxInletDucer = 5;
-        data1.kerInletDucer = 5;
-        data1.kerPintleDucer = 5;
-        data1.loxVenturi = 5;
-        data1.kerVenturi = 5;
-        data1.pneumaticDucer = 5;
-        data1.loadCell = 5;
-        data1.loxRegDucer = 5;
-        data1.kerRegDucer = 5;
-        data1.n2pressDucer = 5;
-        data1.loxTankTC = 5;
-        data1.kerInletTC = 5;
-        data1.kerOutletTC = 5;
-        data1.miscTC = 5;
-        data1.boardTemp = 5;
+        {
+            SensorData data1;
+            data1.loxVent = ECSValveState::OPEN;
+            data1.kerVent = ECSValveState::OPEN;
+            data1.loxDrip = ECSValveState::OPEN;
+            data1.kerDrip = ECSValveState::OPEN;
+            data1.loxPressurant = ECSValveState::OPEN;
+            data1.kerPressurant = ECSValveState::OPEN;
+            data1.loxFlow = ECSValveState::OPEN;
+            data1.kerFlow  = ECSValveState::OPEN;
+            data1.loxPurge = ECSValveState::OPEN;
+            data1.kerPurge = ECSValveState::OPEN;
+            data1.kerOrifice = ECSValveState::OPEN;
 
-        data1.kerInletTC_Fault = 1;
-        data1.kerOutletTC_Fault = 1;
-        data1.loxTankTC_Fault = 1;
-        data1.miscTC_Fault = 1;
+            data1.loxTankDucer = 5;
+            data1.kerTankDucer = 5;
+            data1.purgeDucer = 5;
+            data1.loxInletDucer = 5;
+            data1.kerInletDucer = 5;
+            data1.kerPintleDucer = 5;
+            data1.loxVenturi = 5;
+            data1.kerVenturi = 5;
+            data1.pneumaticDucer = 5;
+            data1.loadCell = 5;
+            data1.loxRegDucer = 5;
+            data1.kerRegDucer = 5;
+            data1.n2pressDucer = 5;
+            data1.loxTankTC = 5;
+            data1.kerInletTC = 5;
+            data1.kerOutletTC = 5;
+            data1.miscTC = 5;
+            data1.boardTemp = 5;
 
-        SensorData data2;
-        data2.loxVent = ECSValveState::CLOSED;
-        data2.kerVent = ECSValveState::CLOSED;
-        data2.loxDrip = ECSValveState::CLOSED;
-        data2.kerDrip = ECSValveState::CLOSED;
-        data2.loxPressurant = ECSValveState::CLOSED;
-        data2.kerPressurant = ECSValveState::CLOSED;
-        data2.loxFlow = ECSValveState::CLOSED;
-        data2.kerFlow  = ECSValveState::CLOSED;
-        data2.loxPurge = ECSValveState::CLOSED;
-        data2.kerPurge = ECSValveState::CLOSED;
-        data2.kerOrifice = ECSValveState::CLOSED;
+            data1.kerInletTC_Fault = 1;
+            data1.kerOutletTC_Fault = 1;
+            data1.loxTankTC_Fault = 1;
+            data1.miscTC_Fault = 1;
+            input.emplace_back(data1);
+        }
 
-        data2.loxTankDucer = 10;
-        data2.kerTankDucer = 10;
-        data2.purgeDucer = 10;
-        data2.loxInletDucer = 10;
-        data2.kerInletDucer = 10;
-        data2.kerPintleDucer = 10;
-        data2.loxVenturi = 10;
-        data2.kerVenturi = 10;
-        data2.pneumaticDucer = 10;
-        data2.loadCell = 10;
-        data2.loxRegDucer = 10;
-        data2.kerRegDucer = 10;
-        data2.n2pressDucer = 10;
-        data2.loxTankTC = 10;
-        data2.kerInletTC = 10;
-        data2.kerOutletTC = 10;
-        data2.miscTC = 10;
-        data2.boardTemp = 10;
+        {
+            SensorData data2;
+            data2.loxVent = ECSValveState::CLOSED;
+            data2.kerVent = ECSValveState::CLOSED;
+            data2.loxDrip = ECSValveState::CLOSED;
+            data2.kerDrip = ECSValveState::CLOSED;
+            data2.loxPressurant = ECSValveState::CLOSED;
+            data2.kerPressurant = ECSValveState::CLOSED;
+            data2.loxFlow = ECSValveState::CLOSED;
+            data2.kerFlow  = ECSValveState::CLOSED;
+            data2.loxPurge = ECSValveState::CLOSED;
+            data2.kerPurge = ECSValveState::CLOSED;
+            data2.kerOrifice = ECSValveState::CLOSED;
 
-        data2.kerInletTC_Fault = 0;
-        data2.kerOutletTC_Fault = 0;
-        data2.loxTankTC_Fault = 0;
-        data2.miscTC_Fault = 0;
+            data2.loxTankDucer = 10;
+            data2.kerTankDucer = 10;
+            data2.purgeDucer = 10;
+            data2.loxInletDucer = 10;
+            data2.kerInletDucer = 10;
+            data2.kerPintleDucer = 10;
+            data2.loxVenturi = 10;
+            data2.kerVenturi = 10;
+            data2.pneumaticDucer = 10;
+            data2.loadCell = 10;
+            data2.loxRegDucer = 10;
+            data2.kerRegDucer = 10;
+            data2.n2pressDucer = 10;
+            data2.loxTankTC = 10;
+            data2.kerInletTC = 10;
+            data2.kerOutletTC = 10;
+            data2.miscTC = 10;
+            data2.boardTemp = 10;
 
-        std::vector<SensorData> input = {data1, data2};
+            data2.kerInletTC_Fault = 0;
+            data2.kerOutletTC_Fault = 0;
+            data2.loxTankTC_Fault = 0;
+            data2.miscTC_Fault = 0;
+            input.emplace_back(data2);
+        }
 
         SensorData result = averageData(input);
 
