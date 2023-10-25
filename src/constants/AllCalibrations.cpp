766f9c8945524b753eb99db3d3636d97b0a78b2d
Kevin Huang <kevin9sd@gmail.com>
2023-10-25 14:26:11 -0400

    calibrations added

--- src/constants/AllCalibrations.cpp
+++ src/constants/AllCalibrations.cpp
@@ -12,8 +12,6 @@
  * anonymous namespace to avoid collisions
  */
 namespace {
-
-
     SensorDataCalibrator kerPintle_calibration(INT_SELECTOR_FUNCT(kerPintleDucer),
                                                IntFuncts::Linear(0.0000742, 26));
 
@@ -26,38 +24,30 @@ namespace {
     SensorDataCalibrator loxTank_calibration(INT_SELECTOR_FUNCT(loxTankDucer),
                                                     IntFuncts::Linear(0.000127, -1.21));
 
-    //IDK upstream of OV
+    SensorDataCalibrator orificeUpstreamDucer_calibration(INT_SELECTOR_FUNCT(orificeUpstreamDucer),
+                                                    IntFuncts::Linear(0.000126,  2.54));
 
     SensorDataCalibrator kerVenturi1_calibration(INT_SELECTOR_FUNCT(kerVenturi),
                                                     IntFuncts::Linear(0.000127, -1.25));
 
-    SensorDataCalibrator kerVenturi2_calibration(INT_SELECTOR_FUNCT(kerVenturi),
+    SensorDataCalibrator kerVenturi2_calibration(INT_SELECTOR_FUNCT(kerVenturi2),
                                                  IntFuncts::Linear(0.000127, -3.11));
 
     SensorDataCalibrator loxVenturi_calibration(INT_SELECTOR_FUNCT(loxVenturi),
                                                  IntFuncts::Linear(0.000128, 5.55));
 
-
     SensorDataCalibrator pnuematicDucer_calibration(INT_SELECTOR_FUNCT(pneumaticDucer),
                                                IntFuncts::Linear(0.0000163, -25.3));
-
-
-
-
-
 }
 
 std::vector<SensorDataCalibrator> calibratorList = {
-//         loxTank_calibration,
-//         kerTank_calibration,
-//         loadCell_calibration,
-//         kerVenturi_calibration,
-//         purgeDucer_calibraton,
-//         loxInletDucer_calibration,
-//         kerPintle_calibration,
-//         kerInlet_calibration,
-//         kerReg_calibration,
-//         loxReg_calibration,
-//         pnuematicDucer_calibration,
-//         n2pressDeucer_calibration
+        kerPintle_calibration,
+        loxReg_calibration,
+        kerReg_calibration,
+        loxTank_calibration,
+        orificeUpstreamDucer_calibration,
+        kerVenturi1_calibration,
+        kerVenturi2_calibration,
+        loxVenturi_calibration,
+        pnuematicDucer_calibration,
 };
