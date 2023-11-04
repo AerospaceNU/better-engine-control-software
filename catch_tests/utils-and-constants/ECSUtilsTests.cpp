29473539813e8d1909efccb7fcdf1edc79981a0a
Kevin Huang <kevin9sd@gmail.com>
2023-11-04 16:13:37 -0400

    remove sleep

--- catch_tests/utils-and-constants/ECSUtilsTests.cpp
+++ catch_tests/utils-and-constants/ECSUtilsTests.cpp
@@ -23,7 +23,7 @@ TEST_CASE("getTimeStamp monotonic time") {
     auto prevTime = getTimeStamp();
 
     for (int i = 0; i < 10; i++){
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        // std::this_thread::sleep_for(std::chrono::milliseconds(10));
         auto curTime = getTimeStamp();
 
         assert(prevTime < curTime);
